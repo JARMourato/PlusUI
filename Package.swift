e993a311c4c45770bf141ee0f07bc7fe6f952666
JARMourato <joao.armourato@gmail.com>
2022-12-05 13:16:22 -0800

    update min version for iOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PlusUI",
-    platforms: [.iOS(.v13), .macOS(.v12), .watchOS(.v6), .tvOS(.v13)],
+    platforms: [.iOS(.v15), .macOS(.v12), .watchOS(.v6), .tvOS(.v15)],
     products: [
         .library(name: "PlusUI", targets: ["PlusUI"]),
     ],
